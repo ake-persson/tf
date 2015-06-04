aaee4fa7e0f45679cb269780c72c0a030a021cff
Michael Persson <michael.ake.persson@gmail.com>
2015-06-04 11:09:55 +0200

    Added template file

--- tf.go
+++ tf.go
@@ -4,7 +4,7 @@ import (
     "os"
     "fmt"
     "log"
-    "path/filepath"
+//    "path/filepath"
     "io/ioutil"
     flags "github.com/jessevdk/go-flags"
     "gopkg.in/yaml.v2"
@@ -19,6 +19,10 @@ func check(e error) {
 }
 
 func main() {
+    // Initialize log
+    l := log.New(os.Stderr, "", 0)
+
+    // Options
     var opts struct {
         Debug bool `short:"d" long:"debug" description:"Debug"`
         Input string `short:"y" long:"input" description:"YAML input"`
@@ -28,17 +32,17 @@ func main() {
         TemplDir string `short:"t" long:"template-dir" description:"Template files with ext. \".tf\" in directory"`
     }
 
+    // Parse options
     _, err := flags.Parse(&opts)
     if err != nil {
         os.Exit(1)
     }
 
-    l := log.New(os.Stderr, "", 0)
-
+    // Get YAML input
     var input []byte
     if opts.Input != "" {
         if opts.InpFile != "default.yaml" {
-            l.Printf("Can't specify both --input and --input-file\n")
+            l.Printf("Can't specify both --input (-y) and --input-file (-i)\n")
             os.Exit(1)
         }
         input = []byte(opts.Input)
@@ -51,47 +55,37 @@ func main() {
         input = c
         check(err)
     }
-//    fmt.Printf("%v\n", input)
 
-    // Decode YAML
+    // Decode YAML 
     var y map[string]interface{}
     err = yaml.Unmarshal(input, &y)
     check(err)
 
-    os.Exit(0)
+    // Template file
+    if opts.TemplFile != "" {
+        if opts.TemplDir != "" {
+            l.Printf("Can't specify both --template-file (-i) and --template-dir (-t)\n")
+            os.Exit(1)
+        }
 
-    files, _ := filepath.Glob(opts.TemplDir + "/*.yaml")
+        if _, err := os.Stat(opts.TemplFile); os.IsNotExist(err) {
+            l.Printf("File doesn't exist: %v\n", opts.TemplFile)
+            os.Exit(1)
+        }
 
-    gy := make(map[string]interface{})
-    for _, file := range files {
-        // Read YAML file
-        fmt.Printf("### Read File ###\n\n%s\n\n", file)
-        c, err := ioutil.ReadFile(file)
+        // Open file
+        c, err := ioutil.ReadFile(opts.TemplFile)
         check(err)
-        if opts.Debug { fmt.Printf("### File Content ###\n\n%s\n", c) }
 
         // Parse template
-        tmpl, err := template.New("template").Parse(string(c))
+        t, err := template.New("template").Parse(string(c))
         check(err)
 
         buf := new(bytes.Buffer)
-        err = tmpl.Execute(buf, gy)
-        check(err)
-        if opts.Debug { fmt.Printf("### Compiled Template ###\n\n%s\n", buf) }
-
-        // Decode YAML
-        var y map[string]interface{}
-            err = yaml.Unmarshal(buf.Bytes(), &y)
+        err = t.Execute(buf, input) 
         check(err)
 
-        // Merge global map
-        for k, v := range y {
-            gy[k] = v
-        }
+        fmt.Printf("%v\n", input)
+        fmt.Printf("%v\n", buf)
     }
-
-    // Result
-    s, err := yaml.Marshal(&gy)
-    check(err)
-    fmt.Printf("### Result ###\n\n%s\n\n", string(s))
 }
