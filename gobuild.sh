b77f6cbcf34b3765947042222237364b454f9659
Michael Persson <michael.ake.persson@gmail.com>
2015-06-10 14:21:05 +0200

    Fix homebrew script

--- gobuild.sh
+++ gobuild.sh
@@ -8,7 +8,7 @@ SRCDIR="${GOPATH}/src/github.com/mickep76/tf"
 [ -d ${GOPATH} ] && rm -rf ${GOPATH}
 mkdir -p ${GOPATH}/{src,pkg,bin}
 mkdir -p ${SRCDIR}
-cp tf.go ${SRCDIR}
+cp *.go ${SRCDIR}
 (
     echo ${GOPATH}
     cd ${SRCDIR}
