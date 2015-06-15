6f2237737f4984caf12ba5e05717145799894790
Michael Persson <michael.ake.persson@gmail.com>
2015-06-15 08:53:29 +0200

    Used gofmt

--- template.go
+++ template.go
@@ -1,166 +1,166 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "reflect"
-    "math/rand"
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strings"
 )
 
 // Convert []interface{} to []string{}
 func arrIntfToStr(inp []interface{}) []string {
-    outp := make([]string, len(inp))
-    for i, val := range inp {
-        outp[i] = fmt.Sprintf("%v", val)
-    }
-    return outp
+	outp := make([]string, len(inp))
+	for i, val := range inp {
+		outp[i] = fmt.Sprintf("%v", val)
+	}
+	return outp
 }
 
 // Convert []string{} to []interface{}
 func arrStrToIntf(inp []string) []interface{} {
-    outp := make([]interface{}, len(inp))
-    for i, val := range inp {
-        outp[i] = val
-    }
-    return outp
+	outp := make([]interface{}, len(inp))
+	for i, val := range inp {
+		outp[i] = val
+	}
+	return outp
 }
 
 // Determine if index is the last element in the array
 func arrLast(i int, inp interface{}) bool {
-    return i == reflect.ValueOf(inp).Len() - 1
+	return i == reflect.ValueOf(inp).Len()-1
 }
 
 // Join elements in an array to a string
 func arrJoin(sep string, inp []interface{}) string {
-    return strings.Join(arrIntfToStr(inp), sep)
+	return strings.Join(arrIntfToStr(inp), sep)
 }
 
 // Split string into an array
 func strSplit(sep string, inp string) []interface{} {
-    return arrStrToIntf(strings.Split(inp, sep))
+	return arrStrToIntf(strings.Split(inp, sep))
 }
 
 // Repeat string x number of times
 func strRepeat(rep int, inp string) string {
-        return strings.Repeat(inp, rep)
+	return strings.Repeat(inp, rep)
 }
 
 // Get keys from interface{}
 func intfKeys(inp interface{}) (interface{}, error) {
-    if inp == nil {
-        return nil, nil
-    }
+	if inp == nil {
+		return nil, nil
+	}
 
-    val := reflect.ValueOf(inp)
-    if val.Kind() != reflect.Map {
-        return nil, fmt.Errorf("Cannot call keys on a non-map value: %v", inp)
-    }
+	val := reflect.ValueOf(inp)
+	if val.Kind() != reflect.Map {
+		return nil, fmt.Errorf("Cannot call keys on a non-map value: %v", inp)
+	}
 
-    vk := val.MapKeys()
-    k := make([]interface{}, val.Len())
-    for i, _ := range k {
-        k[i] = vk[i].Interface()
-    }
+	vk := val.MapKeys()
+	k := make([]interface{}, val.Len())
+	for i, _ := range k {
+		k[i] = vk[i].Interface()
+	}
 
-    return k, nil
+	return k, nil
 }
 
 // Get type (usefull for debugging templates)
 func intfType(inp interface{}) string {
-    return fmt.Sprintf("%v", reflect.TypeOf(inp))
+	return fmt.Sprintf("%v", reflect.TypeOf(inp))
 }
 
 // Test if type is a map i.e. not printable
 func intfIsMap(inp interface{}) bool {
-    return reflect.TypeOf(inp).Kind() == reflect.Map
+	return reflect.TypeOf(inp).Kind() == reflect.Map
 }
 
 // String replace
 func strReplace(oldStr string, newStr string, str string) string {
-    return strings.Replace(str, oldStr, newStr, -1)
+	return strings.Replace(str, oldStr, newStr, -1)
 }
 
 // String trim
 func strTrim(trim string, str string) string {
-    return strings.Trim(str, trim)
+	return strings.Trim(str, trim)
 }
 
 // String trim left
 func strTrimLeft(trim string, str string) string {
-    return strings.TrimLeft(str, trim)
+	return strings.TrimLeft(str, trim)
 }
 
 // String trim right
 func strTrimRight(trim string, str string) string {
-    return strings.TrimRight(str, trim)
+	return strings.TrimRight(str, trim)
 }
 
 // If no value is passed for the second arg. it returns the default
 func intfDefault(def interface{}, inp_opt ...interface{}) interface{} {
-    if len(inp_opt) > 0 {
-        def = inp_opt[0]
-    }
-    return def
+	if len(inp_opt) > 0 {
+		def = inp_opt[0]
+	}
+	return def
 }
 
 func strCenter(size int, str string) string {
-    if size < len(str) {
-        return str
-    }
+	if size < len(str) {
+		return str
+	}
 
-    pad := (size - len(str)) / 2
-    lpad := pad
-    rpad := size - len(str) - lpad
+	pad := (size - len(str)) / 2
+	lpad := pad
+	rpad := size - len(str) - lpad
 
-    return fmt.Sprintf("%s%s%s", strings.Repeat(" ", lpad), str, strings.Repeat(" ", rpad))
+	return fmt.Sprintf("%s%s%s", strings.Repeat(" ", lpad), str, strings.Repeat(" ", rpad))
 }
 
 func intRandom(size int) int {
-    return rand.Intn(size)
+	return rand.Intn(size)
 }
 
 // Capitalize first character in string
 func strCapitalize(str string) string {
-    for i, v := range str {
-        return strings.ToUpper(string(v)) + str[i + 1:]
-    }
-    return ""
+	for i, v := range str {
+		return strings.ToUpper(string(v)) + str[i+1:]
+	}
+	return ""
 }
 
 func intAdd(y int, x int) int {
-    return x + y
+	return x + y
 }
 
 func intSub(y int, x int) int {
-    return x - y
+	return x - y
 }
 
 func intDiv(y int, x int) int {
-    return x / y
+	return x / y
 }
 
 func intMul(y int, x int) int {
-    return x * y
+	return x * y
 }
 
 func strAlignLeft(size int, str string) string {
-    if size < len(str) {
-        return str
-    }
+	if size < len(str) {
+		return str
+	}
 
-    pad := (size - len(str))
+	pad := (size - len(str))
 
-    return fmt.Sprintf("%s%s", str, strings.Repeat(" ", pad))
+	return fmt.Sprintf("%s%s", str, strings.Repeat(" ", pad))
 }
 
 func strAlignRight(size int, str string) string {
-    if size < len(str) {
-        return str
-    }
+	if size < len(str) {
+		return str
+	}
 
-    pad := (size - len(str))
+	pad := (size - len(str))
 
-    return fmt.Sprintf("%s%s", strings.Repeat(" ", pad), str)
+	return fmt.Sprintf("%s%s", strings.Repeat(" ", pad), str)
 }
 
 // odd/even
--- tf.go
+++ tf.go
@@ -1,163 +1,163 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "log"
-    "strings"
-    "io/ioutil"
-    flags "github.com/jessevdk/go-flags"
-    "gopkg.in/yaml.v2"
-    "text/template"
-    "bytes"
-    etcd "github.com/coreos/go-etcd/etcd"
+	"bytes"
+	"fmt"
+	etcd "github.com/coreos/go-etcd/etcd"
+	flags "github.com/jessevdk/go-flags"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"text/template"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 var fns = template.FuncMap{
-    "last": arrLast,
-    "join": arrJoin,
-    "split": strSplit,
-    "repeat": strRepeat,
-    "keys": intfKeys,
-    "type": intfType,
-    "ismap": intfIsMap,
-    "upper": strings.ToUpper,
-    "lower": strings.ToLower,
-    "contains": strings.Contains,
-    "replace": strReplace,
-    "trim": strTrim,
-    "ltrim": strTrimLeft,
-    "rtrim": strTrimRight,
-    "default": intfDefault,
-    "center": strCenter,
-    "random": intRandom,
-    "capitalize": strCapitalize,
-    "add": intAdd,
-    "sub": intSub,
-    "div": intDiv,
-    "mul": intMul,
-    "lalign": strAlignLeft,
-    "ralign": strAlignRight,
+	"last":       arrLast,
+	"join":       arrJoin,
+	"split":      strSplit,
+	"repeat":     strRepeat,
+	"keys":       intfKeys,
+	"type":       intfType,
+	"ismap":      intfIsMap,
+	"upper":      strings.ToUpper,
+	"lower":      strings.ToLower,
+	"contains":   strings.Contains,
+	"replace":    strReplace,
+	"trim":       strTrim,
+	"ltrim":      strTrimLeft,
+	"rtrim":      strTrimRight,
+	"default":    intfDefault,
+	"center":     strCenter,
+	"random":     intRandom,
+	"capitalize": strCapitalize,
+	"add":        intAdd,
+	"sub":        intSub,
+	"div":        intDiv,
+	"mul":        intMul,
+	"lalign":     strAlignLeft,
+	"ralign":     strAlignRight,
 }
 
 func main() {
-    // get the FileInfo struct describing the standard input.
-    fi, _ := os.Stdin.Stat()
+	// get the FileInfo struct describing the standard input.
+	fi, _ := os.Stdin.Stat()
 
-    // Initialize log
-    log := log.New(os.Stderr, "", 0)
+	// Initialize log
+	log := log.New(os.Stderr, "", 0)
 
-    // Options
-    var opts struct {
-        Verbose bool `short:"v" long:"verbose" description:"Verbose"`
-        Version bool `long:"version" description:"Version"`
-        Input string `short:"i" long:"input" description:"YAML input"`
-        InpFile string `short:"f" long:"input-file" description:"YAML input file"`
-        TemplFile string `short:"t" long:"template-file" description:"Template file"`
-        OutpFile string `short:"o" long:"output-file" description:"Output file (STDOUT)"`
-        Permission int `short:"p" long:"permission" description:"Permission for output file" default:"644"`
-        EtcdNode string `short:"n" long:"etcd-node" description:"Etcd Node"`
-        EtcdPort int `short:"P" long:"etcd-port" description:"Etcd Port" default:"2379"`
-        EtcdKey string `short:"k" long:"etcd-key" description:"Etcd Key" default:"/"`
-    }
+	// Options
+	var opts struct {
+		Verbose    bool   `short:"v" long:"verbose" description:"Verbose"`
+		Version    bool   `long:"version" description:"Version"`
+		Input      string `short:"i" long:"input" description:"YAML input"`
+		InpFile    string `short:"f" long:"input-file" description:"YAML input file"`
+		TemplFile  string `short:"t" long:"template-file" description:"Template file"`
+		OutpFile   string `short:"o" long:"output-file" description:"Output file (STDOUT)"`
+		Permission int    `short:"p" long:"permission" description:"Permission for output file" default:"644"`
+		EtcdNode   string `short:"n" long:"etcd-node" description:"Etcd Node"`
+		EtcdPort   int    `short:"P" long:"etcd-port" description:"Etcd Port" default:"2379"`
+		EtcdKey    string `short:"k" long:"etcd-key" description:"Etcd Key" default:"/"`
+	}
 
-    // Parse options
-    _, err := flags.Parse(&opts)
-    check(err)
+	// Parse options
+	_, err := flags.Parse(&opts)
+	check(err)
 
-    // Print version
-    if opts.Version == true {
-        fmt.Printf("%s\n", version)
-        os.Exit(0)
-    }
+	// Print version
+	if opts.Version == true {
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	}
 
-    // Get YAML input
-    var inp []byte
-    if opts.Input != "" {
-        if opts.InpFile != "" {
-            log.Fatal("Can't specify both --input (-i) and --input-file (-f)\n")
-            os.Exit(1)
-        }
-        inp = []byte(opts.Input)
-    } else if opts.InpFile != "" {
-        if _, err := os.Stat(opts.InpFile); os.IsNotExist(err) {
-            log.Printf("File doesn't exist: %v\n", opts.InpFile)
-            os.Exit(1)
-        }
-        content, err := ioutil.ReadFile(opts.InpFile)
-        check(err)
-        inp = content
-    } else {
-        inp = []byte("{}")
-    }
+	// Get YAML input
+	var inp []byte
+	if opts.Input != "" {
+		if opts.InpFile != "" {
+			log.Fatal("Can't specify both --input (-i) and --input-file (-f)\n")
+			os.Exit(1)
+		}
+		inp = []byte(opts.Input)
+	} else if opts.InpFile != "" {
+		if _, err := os.Stat(opts.InpFile); os.IsNotExist(err) {
+			log.Printf("File doesn't exist: %v\n", opts.InpFile)
+			os.Exit(1)
+		}
+		content, err := ioutil.ReadFile(opts.InpFile)
+		check(err)
+		inp = content
+	} else {
+		inp = []byte("{}")
+	}
 
-    // Decode YAML 
-    var y map[string]interface{}
-    err = yaml.Unmarshal(inp, &y)
-    check(err)
+	// Decode YAML
+	var y map[string]interface{}
+	err = yaml.Unmarshal(inp, &y)
+	check(err)
 
-    // Get environment
-    env := make(map[string]string)
-    for _, e := range os.Environ() {
-        v := strings.Split(e, "=")
-        env[v[0]] = v[1]
-    }
+	// Get environment
+	env := make(map[string]string)
+	for _, e := range os.Environ() {
+		v := strings.Split(e, "=")
+		env[v[0]] = v[1]
+	}
 
-    y["Env"] = env
+	y["Env"] = env
 
-//    s, err := yaml.Marshal(&y)
-//    fmt.Printf("%s\n", string(s))
+	//    s, err := yaml.Marshal(&y)
+	//    fmt.Printf("%s\n", string(s))
 
-    vars := make(map[interface{}]interface{})
-    if opts.EtcdNode != "" {
-        node := []string{fmt.Sprintf("http://%v:%v", opts.EtcdNode, opts.EtcdPort)}
-        client := etcd.NewClient(node)
-        res, _ := client.Get(opts.EtcdKey, true, true)
-        etcdNestedMap(res.Node, vars)
-        y["Etcd"] = vars
-    }
+	vars := make(map[interface{}]interface{})
+	if opts.EtcdNode != "" {
+		node := []string{fmt.Sprintf("http://%v:%v", opts.EtcdNode, opts.EtcdPort)}
+		client := etcd.NewClient(node)
+		res, _ := client.Get(opts.EtcdKey, true, true)
+		etcdNestedMap(res.Node, vars)
+		y["Etcd"] = vars
+	}
 
-//    s, err := yaml.Marshal(&y)
-//    fmt.Printf("%s\n", string(s))
+	//    s, err := yaml.Marshal(&y)
+	//    fmt.Printf("%s\n", string(s))
 
-    // Template input
-    var templ string
-    if (fi.Mode() & os.ModeCharDevice) == 0 {
-        bytes, _ := ioutil.ReadAll(os.Stdin)
-        templ = string(bytes)
-    } else if opts.TemplFile != "" {
-        if _, err := os.Stat(opts.TemplFile); os.IsNotExist(err) {
-            log.Printf("File doesn't exist: %v\n", opts.TemplFile)
-            os.Exit(1)
-        }
+	// Template input
+	var templ string
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		bytes, _ := ioutil.ReadAll(os.Stdin)
+		templ = string(bytes)
+	} else if opts.TemplFile != "" {
+		if _, err := os.Stat(opts.TemplFile); os.IsNotExist(err) {
+			log.Printf("File doesn't exist: %v\n", opts.TemplFile)
+			os.Exit(1)
+		}
 
-        // Open file
-        c, err := ioutil.ReadFile(opts.TemplFile)
-        check(err)
-        templ = string(c)
-    } else {
-        log.Printf("No template specified using --template-file (-t) or piped to STDIN\n")
-        os.Exit(1)
-    }
+		// Open file
+		c, err := ioutil.ReadFile(opts.TemplFile)
+		check(err)
+		templ = string(c)
+	} else {
+		log.Printf("No template specified using --template-file (-t) or piped to STDIN\n")
+		os.Exit(1)
+	}
 
-    // Parse template
-    t := template.Must(template.New("template").Funcs(fns).Parse(templ))
+	// Parse template
+	t := template.Must(template.New("template").Funcs(fns).Parse(templ))
 
-    buf := new(bytes.Buffer)
-    err = t.Execute(buf, y)
-    check(err)
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, y)
+	check(err)
 
-    // Write result
-    if opts.OutpFile != "" {
-        err := ioutil.WriteFile(opts.OutpFile, buf.Bytes(), os.FileMode(opts.Permission))
-        check(err)
-    } else {
-        fmt.Printf("%v\n", buf)
-    }
+	// Write result
+	if opts.OutpFile != "" {
+		err := ioutil.WriteFile(opts.OutpFile, buf.Bytes(), os.FileMode(opts.Permission))
+		check(err)
+	} else {
+		fmt.Printf("%v\n", buf)
+	}
 }
