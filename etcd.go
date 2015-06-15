6f8f7b3996059ac9cfb60ccf257fa50e7b53cdc5
Michael Persson <michael.ake.persson@gmail.com>
2015-06-15 09:10:25 +0200

    TEST

--- etcd.go
+++ etcd.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "strings"
-    etcd "github.com/coreos/go-etcd/etcd"
+	etcd "github.com/coreos/go-etcd/etcd"
+	"strings"
 )
 
 // Return Etcd structure as nested map[interface{}]interface{}
 func etcdNestedMap(node *etcd.Node, data map[interface{}]interface{}) {
-    for _, node := range node.Nodes {
-        keys := strings.Split(node.Key, "/")
-        key := keys[len(keys) - 1]
-        if node.Dir {
-            data[key] = make(map[interface{}]interface{})
-            etcdNestedMap(node, data[key].(map[interface{}]interface{}))
-        } else {
-            data[key] = node.Value
-        }
-    }
+	for _, node := range node.Nodes {
+		keys := strings.Split(node.Key, "/")
+		key := keys[len(keys)-1]
+		if node.Dir {
+			data[key] = make(map[interface{}]interface{})
+			etcdNestedMap(node, data[key].(map[interface{}]interface{}))
+		} else {
+			data[key] = node.Value
+		}
+	}
 }
