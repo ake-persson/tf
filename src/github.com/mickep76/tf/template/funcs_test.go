a9c800bf9df7e5c09c2cb183613b8fa9537418b2
Michael Persson <michael.ake.persson@gmail.com>
2015-07-31 16:45:41 +0200

    Removed dead code

--- src/github.com/mickep76/tf/template/funcs_test.go
+++ src/github.com/mickep76/tf/template/funcs_test.go
@@ -27,17 +27,3 @@ func Test_Last(t *testing.T) {
 	}
 
 }
-
-/*
-func Test_Add2Ints_1(t *testing.T) { //test function starts with "Test" and takes a pointer to type testing.T
-    if (Add2Ints(3, 4) != 7) { //try a unit test on function
-        t.Error("Add2Ints did not work as expected.") // log error if it did not work as expected
-    } else {
-        t.Log("one test passed.") // log some info if you want
-    }
-}
-
-func Test_Add2Ints_2(t *testing.T) { //test function starts with "Test" and takes a pointer to type testing.T
-    t.Error("this is just hardcoded as an error.") //Indicate that this test failed and log the string as info
-}
-*/
